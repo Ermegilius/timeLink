6952a8ca7c810db3f4a88b77bf2bc13c384c620f
Vladimir Beliakov <ermegilius@gmail.com>
2024-12-11 17:37:09 +0200

    add: music player component

---
+++ src/components/MusicPlayer/MusicPlayer.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import AudioPlayer from 'react-h5-audio-player';
+import 'react-h5-audio-player/lib/styles.css';
+import './MusicPlayer.css';
+
+
+const MusicPlayer = () => (
+    <AudioPlayer
+        autoPlay
+        src='https://cdn.pixabay.com/audio/2021/08/22/audio_9a17017104.mp3'
+        volume={0.3}
+        loop={true}
+        showFilledVolume={true}  
+    />
+);
+
+export default MusicPlayer;
+
+// Track info:
+// Author: Zen_Man
+// Track name: Modular Ambient 06 - Endless Sequence
+// source: https://pixabay.com/music/pulses-modular-ambient-06-endless-sequence-7597/
