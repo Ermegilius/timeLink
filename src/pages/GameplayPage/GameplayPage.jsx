01d03c19dedc9ed5aeb402d1dd6e1270e9ac1689
Beliakov Vladimir <ermegilius@gmail.com>
2024-12-13 12:23:25 +0200

    add: finish button and gamelplay finish logic

--- src/pages/GameplayPage/GameplayPage.jsx
+++ src/pages/GameplayPage/GameplayPage.jsx
@@ -10,6 +10,7 @@ import Modal from "../../components/GamePlay/Modal/Modal";
 import Button from "../../components/Button/Button";
 import MusicPlayer from "../../components/MusicPlayer/MusicPlayer"; //music player component
 import { AuthContext } from "../../providers/AuthProvider";
+import FinishedGameBanner from "../../components/FinishedGameBanner/FinishedGameBanner";
 
 const gameplayFields = [116, 87, 200, 48, 212, 120, 205, 52, 165, 82, 223, 154]; // Playable fields
 const gameplayTimes = [
@@ -34,6 +35,7 @@ function GameplayPage({ onLogOut }) {
   const [icons, setIcons] = useState(Array(10).fill(lockIcon)); // Initialize with default icons
 
   const [isModalOpen, setIsModalOpen] = useState(false); //modal is closed by default
+  const [isGameFinished, setIsGameFinished] = useState(false); // Track if the game is finished
   const openModal = () => setIsModalOpen(true);
   const closeModal = () => setIsModalOpen(false);
 
@@ -60,97 +62,102 @@ function GameplayPage({ onLogOut }) {
     handleMoveNext(); // Automatically move the avatar to the next grid
   };
 
+  // Function to finish the game
+  const finishGame = () => {
+    setIsGameFinished(true);
+  };
+
+  // Call finishGame when the game is finished
+  useEffect(() => {
+    if (currentFieldIndex === gameplayFields.length - 1) {
+      finishGame();
+    }
+  }, [currentFieldIndex]);
+
   return (
     <main>
-      <div className="boxForGameplayAndMenu flex flex-row flex-nowrap gap-12 w-full h-[79.8vh]">
-        <div className="boxForGameplayAndItems flex flex-col flex-nowrap w-full h-full relative z-20 border-2 border-[#8168fe] rounded-md shadow-[0_4px_4px_rgba(0,0,0,0.25),0_3px_6px_rgba(0,0,0,0.22)]">
-          <div className="boxForGameplay block h-full w-full">
-            <div className="playboard h-80 w-full z-10 absolute opacity-100">
-              {
-                <img
-                  className="playboard-img"
-                  src={boardImage}
-                  alt="game playboard"
-                />
-              }
+      {isGameFinished ? (
+        <FinishedGameBanner />
+      ) : (
+        <div className="boxForGameplayAndMenu flex flex-row flex-nowrap gap-12 w-full h-[79.8vh]">
+          <div className="boxForGameplayAndItems flex flex-col flex-nowrap w-full h-full relative z-20 border-2 border-[#8168fe] rounded-md shadow-[0_4px_4px_rgba(0,0,0,0.25),0_3px_6px_rgba(0,0,0,0.22)]">
+            <div className="boxForGameplay block h-full w-full">
+              <div className="playboard h-80 w-full z-10 absolute opacity-100">
+                {
+                  <img
+                    className="playboard-img"
+                    src={boardImage}
+                    alt="game playboard"
+                  />
+                }
+              </div>
+              <div
+                className="playboard-grid grid grid-cols-14 gap-0 h-full w-full z-20"
+                id="playboard-grid"
+              >
+                {[...Array(294)].map((_, index) => (
+                  <div
+                    key={index}
+                    id={index + 1}
+                    className="relative text-[0.7rem] text-transparent text-center max-w-[75px]"
+                  >
+                    {index + 1}
+                  </div>
+                ))}
+              </div>
             </div>
             <div
-              className="playboard-grid grid grid-cols-14 gap-0 h-full w-full z-20"
-              id="playboard-grid"
+              id="itemList"
+              className="itemList flex flex-row flex-nowrap w-full bg-[#d9f9cf] border-t-2 border-[#8168fe] rounded-b-md"
             >
-              {[...Array(294)].map((_, index) => (
+              {icons.map((icon, index) => (
                 <div
                   key={index}
-                  id={index + 1}
-                  className="relative text-[0.7rem] text-transparent text-center max-w-[75px]"
+                  className="item flex justify-center items-center bg-[#fefffa] h-[70px] w-[10%] p-2 border-l border-r border-[#8168fe]"
                 >
-                  {index + 1}
+                  <img src={icon} className="lockIcon w-[40px] mx-4" alt="icon" />
                 </div>
               ))}
             </div>
           </div>
-          <div
-            id="itemList"
-            className="itemList flex flex-row flex-nowrap w-full bg-[#d9f9cf] border-t-2 border-[#8168fe] rounded-b-md"
-          >
-            {icons.map((icon, index) => (
-              <div
-                key={index}
-                className="item flex justify-center items-center bg-[#fefffa] h-[70px] w-[10%] p-2 border-l border-r border-[#8168fe]"
-              >
-                <img src={icon} className="lockIcon w-[40px] mx-4" alt="icon" />
-              </div>
-            ))}
-          </div>
-        </div>
-        <aside className="boxForMenu flex flex-col items-center justify-evenly text-center w-[20%] h-full bg-[#bbbbff] rounded-md border-2 border-[#8168fe] p-5 shadow-[0_4px_4px_rgba(0,0,0,0.25),0_3px_6px_rgba(0,0,0,0.22)]">
-          <div id="user" className="user flex flex-col items-center">
-            <img
-              src={userPic}
-              className="align-self-end w-[180px] max-h-[80px] pt-2 pr-4 pl-2 bg-[#fefffa] rounded-t-md border-b-[1px] border-l-2 border-r-2 border-t-2 border-[#8168fe]"
-              alt="little Alien"
-            />
-            <h3
-              id="userName"
-              className="text-[16px] bg-[#fefffa] w-[180px] py-0.5 rounded-b-md border-b-2 border-l-2 border-r-2 border-[#8168fe] shadow-[0_4px_4px_rgba(0,0,0,0.25)]"
-            >
-              <div>
-                {user ? user.displayName || user.email : "Guest Player"}
-              </div>
-            </h3>
-          </div>
-          <div
-            id="gameProgress"
-            className="gameProgress h-38 w-[180px] mt-4 mb-0 p-1 bg-[#fefffa] rounded-md border-2 border-[#8168fe] shadow-[0_4px_4px_rgba(0,0,0,0.25)]"
-          >
-            <p>{`You are in ${gameplayTimes[currentFieldIndex]}`}</p>
-          </div>
-          <div>
-            <Button text="Next riddle" onClick={openModal} className="my-0" />
-            <Modal
-              isOpen={isModalOpen}
-              onClose={closeModal}
-              handleMoveNext={handleMoveNext}
-              setRewards={setRewards}
-              handleCorrectAnswer={handleCorrectAnswer}
-            />
-          </div>
-          <div className="musicBox w-[180px] h-32 p-1 bg-[#fefffa] rounded-md border-2 border-[#8168fe] shadow-[0_4px_4px_rgba(0,0,0,0.25)]">
-            <MusicPlayer />
-          </div>
-
-          {/* <Button text="Exit" onClick={onLogOut} data-role="primary" className="mt-6"/>
-          <div id="settings">
-            <a href="#">
+          <aside className="boxForMenu flex flex-col items-center justify-evenly text-center w-[20%] h-full bg-[#bbbbff] rounded-md border-2 border-[#8168fe] p-5 shadow-[0_4px_4px_rgba(0,0,0,0.25),0_3px_6px_rgba(0,0,0,0.22)]">
+            <div id="user" className="user flex flex-col items-center">
               <img
-                src={musicIcon}
-                className="musicIcon max-w-[60px] mt-4"
-                alt="music icon"
+                src={userPic}
+                className="align-self-end w-[180px] max-h-[80px] pt-2 pr-4 pl-2 bg-[#fefffa] rounded-t-md border-b-[1px] border-l-2 border-r-2 border-t-2 border-[#8168fe]"
+                alt="little Alien"
               />
-            </a>
-          </div>*/}
-        </aside>
-      </div>
+              <h3
+                id="userName"
+                className="text-[16px] bg-[#fefffa] w-[180px] py-0.5 rounded-b-md border-b-2 border-l-2 border-r-2 border-[#8168fe] shadow-[0_4px_4px_rgba(0,0,0,0.25)]"
+              >
+                <div>
+                  {user ? user.displayName || user.email : "Guest Player"}
+                </div>
+              </h3>
+            </div>
+            <div
+              id="gameProgress"
+              className="gameProgress h-38 w-[180px] mt-4 mb-0 p-1 bg-[#fefffa] rounded-md border-2 border-[#8168fe] shadow-[0_4px_4px_rgba(0,0,0,0.25)]"
+            >
+              <p>{`You are in ${gameplayTimes[currentFieldIndex]}`}</p>
+            </div>
+            <div>
+              <Button text={currentFieldIndex !== 11 ? "Next riddle" : "Finish game"} onClick={currentFieldIndex !==11 ?openModal : finishGame} className="my-0" />
+              <Modal
+                isOpen={isModalOpen}
+                onClose={closeModal}
+                handleMoveNext={handleMoveNext}
+                setRewards={setRewards}
+                handleCorrectAnswer={handleCorrectAnswer}
+              />
+            </div>
+            <div className="musicBox w-[180px] h-32 p-1 bg-[#fefffa] rounded-md border-2 border-[#8168fe] shadow-[0_4px_4px_rgba(0,0,0,0.25)]">
+              <MusicPlayer />
+            </div>
+          </aside>
+        </div>
+      )}
     </main>
   );
 }
