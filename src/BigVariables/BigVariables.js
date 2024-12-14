5195aef6835980af7779252099e252ff54dbdb3f
Jana Stefanie <s2400789@BC-CWRW4DVTW1-Kannettava-tietokone.local>
2024-12-14 21:17:52 +0200

    fix timeline times, fix close modal when clicking x, fix fontsize and color inside modal & correct timeout for text in modal when answer

--- src/BigVariables/BigVariables.js
+++ src/BigVariables/BigVariables.js
@@ -1,4 +1,5 @@
 const GameplayTimes = [
+    "the very early stage of the forming Earth. This is a fun place, let's find out more about it!",
     "Archean: You see a water world with a little bit of land. There are microbial mats living here.",
     "Proterozoic: You see the first multicellular life forms in the gigantic oceans.",
     "Paleozoic: You witness the Cambrian explosion, you see fish, amphibians, and reptiles.",
@@ -7,8 +8,8 @@ const GameplayTimes = [
     "Neolithic: You see large forests, rivers, and bottomlands. There are houses, people are farming and trading.",
     "Iron Age: The production of smelted iron is everywhere. People are living in cities, having temples and water pipes.",
     "15th century: Banking and accounting are founded, the invention of the movable type begins the printing press.",
-    "17th century: Early modern period where big cities and kingdoms are. The microscope is invented.",
-    "1879: You are in Thomas Edisons lab in New Jersey. He just invented an incandescent light bulb.",
+    //"17th century: Early modern period where big cities and kingdoms are. The microscope is invented.",
+    //"1879: You are in Thomas Edisons lab in New Jersey. He just invented an incandescent light bulb.",
     "1969: You are witnessing the first time people left the Earth to land on the moon—explorers like you!",
     "2007: You are a visitor at the Macworld keynote where Steve Jobs introduces the iPhone, the first smartphone.",
 ];
